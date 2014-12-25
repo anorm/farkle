42cd909dc56d57f51968a5de06b63c151180aaaf
Anders Norman <anders@norman.info>
2014-12-25 23:32:40 +0000

    Implemented Game.valueOf

--- farkle.py
+++ farkle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import random
+import collections
 
 class Dice:
     value = 0
@@ -14,25 +15,73 @@ class Dice:
 class Turn:
     def __init__(self):
         self.dice = [Dice() for x in xrange(6)]
+
+    def take(self):
+        if len(self.dice) == 0:
+            return False
         for d in self.dice:
             d.roll()
+        self.dice = sorted(self.dice, key=lambda x: x.value)
+        if Game.valueOf(self.dice) == 0:
+            return False
+        return True
 
 class Game:
     def __init__(self):
-        pass
+        self.players = []
+        
+    def addPlayer(self, player):
+        self.players.append(player)
+    
+    def run(self):
+        for player in self.players:
+            player.score = 0
+            
+        while max(player.score for player in self.players) < 10000:
+            for player in self.players:
+                turn = Turn()
+                player.takeTurn(turn)
+                
+                player.score += 1000
 
     @staticmethod
     def valueOf(diceList):
-        sortedDice = sorted(diceList, key=lambda x: x.value)
-        return sortedDice
+        sortedDice = sorted([d.value for d in diceList])
+        counter = collections.Counter(sortedDice)
+        if len(sortedDice) == 6:
+            if len(counter) == 6: # straight
+                return 1500
+            if max(counter.values()) == 2 and min(counter.values()) == 2: # three pairs
+                return 2000
+        sum = 0
+        for n,c in counter.most_common():
+            val = 0
+            if c >= 3:
+                if n == 1:
+                    n = 10
+                val = (n * 100) * pow(2, c - 3)
+            elif n == 1:
+                val = c * 100
+            elif n == 5:
+                val = c * 50
+            sum += val
+        return sum
 
 
 class AIPlayer:
     def __init__(self):
         pass
+    
+    def takeTurn(self, turn):
+        result = turn.take()
+        print("Taking turn:")
+        print("  Got:       {0}".format(turn.dice))
+        print("  Result:    {0}".format(result))
+        print("  Value:     {0}".format(Game.valueOf(turn.dice)))
+        
 
+game = Game()
+game.addPlayer(AIPlayer())
+game.run()
 
-turn = Turn()
-
-print(Game.valueOf(turn.dice))
 
